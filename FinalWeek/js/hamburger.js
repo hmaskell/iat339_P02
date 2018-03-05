5008b32b653b10b1b170ffcc3f5cbe8f106eb5f9
Heather <hmaskell@sfu.ca>
2018-03-04 23:30:11 -0800

    Update Search

--- FinalWeek/js/hamburger.js
+++ FinalWeek/js/hamburger.js
@@ -2,6 +2,7 @@
 
       window.onresize = setMenu;
       var menu = document.getElementById('menuToggle');
+      var search = document.getElementById('form-search');
       if (menu != null) menu.style.display = 'none';
 
       /* inspired by https://only.beaverjunction.com/animated-hamburger-icon-for-the-beaver-builder-theme/ */
@@ -12,6 +13,15 @@
             }
             else {
               menu.style.display = 'none';
+            }
+        }
+
+        function search(x) {
+          if (search.style.display == null || search.style.display == 'none'){
+              search.style.display = 'inline-block';
+            }
+            else {
+              search.style.display = 'none';
             }
         }
 
